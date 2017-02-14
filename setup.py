7a5e55ff75b01f9a4a03d6de95b53903d64b0038
Paul Hummer <paul@eventuallyanyway.com>
2017-02-13 18:42:36 -0700

    Add files needed for test and release

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name="puff",
+    version="0.1",
+    description="API Validation for SQLAlchemy-jsonapi",
+    maintainer="Paul Hummer",
+    maintainer_email="paul@eventuallyanyway.com",
+    url="https://github.com/rockstar/puff",
+    license="MIT",
+    install_requires=['sqlalchemy'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+    ],
+)
